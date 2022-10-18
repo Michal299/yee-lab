d4c37d76b3173440e465356e9a067514ed91a7dd
Michał Błajet <michal.blajet00@gmail.com>
2022-10-18 12:28:20 +0200

    WIP

---
+++ src/main/java/pl/edu/pg/s180564/view/Config.java
@@ -0,0 +1,13 @@
+package pl.edu.pg.s180564.view;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+/**
+ * Global config for JSF.
+ */
+@FacesConfig
+@ApplicationScoped
+public class Config {
+
+}
